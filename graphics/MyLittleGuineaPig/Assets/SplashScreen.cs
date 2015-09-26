3b412ba5401a509f9caab30fdb0b00a75866873d
Birgit Pohl <lintusotarauha@googlemail.com>
2015-09-26 15:37:31 +0200

    Inspiration of My Little Guinea Pig

---
+++ graphics/MyLittleGuineaPig/Assets/SplashScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GuineaPig.Health = new PigHealthProperty();
+		GuineaPig.Cuteness = new PigCutenessProperty();
+		GuineaPig.Mood = new PigMoodProperty();
+		GuineaPig.Fullness = new PigFullnessProperty();
+		GuineaPig.Radioactivity = new PigRadioactivityProperty();
+		GuineaPig.Genpurity = new PigPurityOfGenProperty();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LoadGame() {
+		Application.LoadLevel("Stage1");
+	}
+}
