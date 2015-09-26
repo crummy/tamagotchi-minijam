cf8e0ecadc56de047673b38d49a29fe1772ae7ce
Raffael Zica <raffael@pixeltastic.de>
2015-09-26 17:43:58 +0200

    Added more frontend

---
+++ src/frontend.js
@@ -0,0 +1,54 @@
+function onPet()
+{
+        alert("blah");
+}
+
+function onSleep()
+{
+        alert("blah");
+}
+
+function onToilet()
+{
+        alert("blah");
+}
+
+function onClubbing()
+{
+        alert("blah");
+}
+
+function onBurger()
+{
+        alert("blah");
+}
+
+function onVegan()
+{
+        alert("blah");
+}
+
+function onBeer()
+{
+        alert("blah");
+}
+
+function onEdamame()
+{
+        alert("blah");
+}
+
+function onSweets()
+{
+        alert("blah");
+}
+
+function onQuinoa()
+{
+        alert("blah");
+}
+
+function onDoener()
+{
+        alert("blah");
+}
