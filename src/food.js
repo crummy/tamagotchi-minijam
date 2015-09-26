7e8b38f085971cb9ae1134b983a8c61ab2e718b9
Raffael Zica <raffael@pixeltastic.de>
2015-09-26 19:43:19 +0200

    food

--- src/food.js
+++ src/food.js
@@ -5,10 +5,10 @@ function Food(name, energy, hunger, health) {
     this.health = health;
 }
 
-var burger = new Food("Burger", -5, -10, 1);
+var burger = new Food("Burger", 5, -10, -5);
 var waffles = new Food("Vegan Corn Waffles", 10, -1, -3);
 var beer = new Food("Microbrew Beer", 15, 5, -5);
-var quinoa = new Food("Quinoa", 15, -1, 1);
-var edamame = new Food("Edamame", 5, -2, 2);
+var quinoa = new Food("Quinoa", -15, -1, 1);
+var edamame = new Food("Edamame", -5, -2, 2);
 var sweets = new Food("Sweets", 5, 0, -5);
 var doner = new Food("Doner Kebap", 10, -5, -10);
